bef9857700ab65b1088b9c0265930ff8cf9fa176
entlog <oscar.ohv@gmail.com>
2025-01-15 18:56:10 +0100

    main (changed test)

--- test/commands/prenent/package/merge.test.ts
+++ test/commands/prenent/package/merge.test.ts
@@ -15,26 +15,5 @@ describe('prenent package merge', () => {
     $$.restore();
   });
 
-  it('runs hello', async () => {
-    await PrenentPackageMerge.run([]);
-    const output = sfCommandStubs.log
-      .getCalls()
-      .flatMap((c) => c.args)
-      .join('\n');
-    expect(output).to.include('hello world');
-  });
 
-  it('runs hello with --json and no provided name', async () => {
-    const result = await PrenentPackageMerge.run([]);
-    expect(result.path).to.equal('src/commands/prenent/package/merge.ts');
-  });
-
-  it('runs hello world --name Astro', async () => {
-    await PrenentPackageMerge.run(['--name', 'Astro']);
-    const output = sfCommandStubs.log
-      .getCalls()
-      .flatMap((c) => c.args)
-      .join('\n');
-    expect(output).to.include('hello Astro');
-  });
 });
